fd7945aa95a5210c28a41ff5a95d3db579be4530
jtranberg <35013712+jtranberg@users.noreply.github.com>
2024-02-01 15:52:33 -0800

    security

--- src/pages/Candles.js
+++ src/pages/Candles.js
@@ -12,23 +12,23 @@ class Socials extends React.Component {
 
         <div class="thumbnails">
         <div class="card"> Scented Candles: Fire for your Inner Being
-        <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Candles,scented ">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Candles,scented ">
             <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/81FLPQ4ZI7L._AC_UL320_.jpg" alt="link"/></a>
         </div>
         <div class="card">Astology Candles:Elightenment and empowering
-        <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Candles,astrology ">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Candles,astrology ">
             <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/8140CbFubTL._AC_UL320_.jpg"alt="link" /></a>
       </div>
         <div class="card">Colored Candles:  For your magical needs
-          <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Colored candles ">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Colored candles ">
             <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/71x3xV5X-pL._AC_UL320_.jpg" alt="link"/></a>
       </div>
         <div class="card">Unscented Candles: When you just need the fire light
-          <a target="_blank" rel="noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Unscented candles ">
+          <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Unscented candles ">
             <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/51gEH9LPr1L._AC_UL320_.jpg" alt="link"/></a>
       </div>
         <div class="card">Bees Wax Candles:An alternative fire source
-          <a target="_blank"rel="noreferrer"  href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Bees wax candles ">
+          <a target="_blank"rel="noopener noreferrer"  href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Bees wax candles ">
             <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/91eFH5ASYZL._AC_UL320_.jpg" alt="link"/></a>
       </div>
       
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -12,24 +12,24 @@ class Socials extends React.Component {
 
     <div class="thumbnails">
             <div class="card">Somavedic:Frequency generator
-            <a  href="/somavedic">
-                <img class="thumb1" id="pic" src="https://somavedic.ca/cdn/shop/products/Sky_540x_5ed71b18-9d1b-49f3-80ef-5516ab2f72e9_400x.jpg?v=1624104654"  alt="Somavedic" /></a>
+            <a  href="/somavedic" rel="noopener noreferrer">
+                <img   class="thumb1" id="pic" src="https://somavedic.ca/cdn/shop/products/Sky_540x_5ed71b18-9d1b-49f3-80ef-5516ab2f72e9_400x.jpg?v=1624104654"  alt="Somavedic" /></a>
            </div> 
            <div class="card" >Crystals CLeansing and Regenerating
-            <a  href="/crystals">
+            <a  href="/crystals" rel="noopener noreferrer">
                 <img   class="thumb1" id="pic" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-ZS35-j1ASubzUFCsCuFKZBjBaQ6ZhLlB8XIXR3iNf8UTfkZ1nsKKzfexWg7wFbZFxg&usqp=CAU" alt="Crystals" /></a>
            </div>
            <div class="card">Candles:Magical or Scented
-            <a  href="/candles">
+            <a  href="/candles" rel="noopener noreferrer">
                 <img   class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/71+6fqol6OL._AC_UL320_.jpg" alt="Candles" /></a>
            </div>
            <div class="card">Suppliments:For A Better Inner Being
-            <a  href="/supplements"
-            ><img   class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/51YSRYZ66sL._AC_UL320_.jpg" alt="Supplements" /></a>
+            <a  href="/supplements" rel="noopener noreferrer" >
+            <img   class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/51YSRYZ66sL._AC_UL320_.jpg" alt="Supplements" /></a>
            </div>
            <div class="card">EMF:
            For A Better Inner Being
-            <a  href="/emf">
+            <a  href="/emf" rel="noopener noreferrer">
                 <img   class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/617uNoQtDwL._AC_UY218_.jpg" alt="EMF" /></a>
            </div>
     </div>
