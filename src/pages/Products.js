f8af02e61f06a08d3fd903c739fb2e0a5de132c4
jtranberg <35013712+jtranberg@users.noreply.github.com>
2024-02-01 16:38:29 -0800

    l

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,39 +1,39 @@
 import React from "react";
 import "../components/socials.css"
 import  "../components/products.css"
-import {Link} from "react-router-dom"
 
-class Socials extends React.Component {
+
+class Products extends React.Component {
   render() {
     return (
         <div>
 
-        <h1 class="banner">Recommended Products For Your Inner Being</h1>
+        <h1 className="banner">Recommended Products For Your Inner Being</h1>
 
-    <div class="thumbnails">
-            <div class="card">Somavedic:Frequency generator
+    <div className="thumbnails">
+            <div className="card">Somavedic:Frequency generator
             <a  href="/somavedic" rel="noopener noreferrer">
-                <img   class="thumb1" id="pic" src="https://somavedic.ca/cdn/shop/products/Sky_540x_5ed71b18-9d1b-49f3-80ef-5516ab2f72e9_400x.jpg?v=1624104654"  alt="Somavedic" /></a>
+                <img   className="thumb1" id="pic" src="https://somavedic.ca/cdn/shop/products/Sky_540x_5ed71b18-9d1b-49f3-80ef-5516ab2f72e9_400x.jpg?v=1624104654"  alt="Somavedic" /></a>
            </div> 
-           <div class="card" >Crystals CLeansing and Regenerating
+           <div className="card" >Crystals CLeansing and Regenerating
             <a  href="/crystals" rel="noopener noreferrer">
-                <img   class="thumb1" id="pic" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-ZS35-j1ASubzUFCsCuFKZBjBaQ6ZhLlB8XIXR3iNf8UTfkZ1nsKKzfexWg7wFbZFxg&usqp=CAU" alt="Crystals" /></a>
+                <img   className="thumb1" id="pic" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-ZS35-j1ASubzUFCsCuFKZBjBaQ6ZhLlB8XIXR3iNf8UTfkZ1nsKKzfexWg7wFbZFxg&usqp=CAU" alt="Crystals" /></a>
            </div>
-           <div class="card">Candles:Magical or Scented
-            <Link to="./Candles" rel="noopener noreferrer">
-                <img   class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/71+6fqol6OL._AC_UL320_.jpg" alt="Candles" /></Link>
+           <div className="card">Candles:Magical or Scented
+            <a  href="./candles/Candles" rel="noopener noreferrer">
+                <img   className="thumb1" id="pic" src="https://m.media-amazon.com/images/I/71+6fqol6OL._AC_UL320_.jpg" alt="Candles" /></a>
            </div>
-           <div class="card">Suppliments:For A Better Inner Being
+           <div className="card">Suppliments:For A Better Inner Being
             <a  href="/supplements" rel="noopener noreferrer" >
-            <img   class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/51YSRYZ66sL._AC_UL320_.jpg" alt="Supplements" /></a>
+            <img   className="thumb1" id="pic" src="https://m.media-amazon.com/images/I/51YSRYZ66sL._AC_UL320_.jpg" alt="Supplements" /></a>
            </div>
-           <div class="card">EMF:
+           <div className="card">EMF:
            For A Better Inner Being
             <a  href="/emf" rel="noopener noreferrer">
-                <img   class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/617uNoQtDwL._AC_UY218_.jpg" alt="EMF" /></a>
+                <img   className="thumb1" id="pic" src="https://m.media-amazon.com/images/I/617uNoQtDwL._AC_UY218_.jpg" alt="EMF" /></a>
            </div>
     </div>
-<section class="paragraph">
+<section className="paragraph">
     
         "These products are wht we recomend"
     </section>
@@ -41,4 +41,4 @@ class Socials extends React.Component {
     )
   }
 }
-  export default Socials;
+  export default Products;
--- src/pages/candles/Candles.js
+++ src/pages/candles/Candles.js
@@ -1,35 +1,32 @@
 import React from "react";
-import "../components/socials.css"
-import  "../components/products.css"
 
-
-class Socials extends React.Component {
+class Candles extends React.Component {
   render() {
     return (
         <div>
 
-<h1 class="banner">Fire Alumination For Your Inner Being!</h1>
+<h1 className="banner">Fire Alumination For Your Inner Being!</h1>
 
-        <div class="thumbnails">
-        <div class="card"> Scented Candles: Fire for your Inner Being
+        <div className="thumbnails">
+        <div className="card"> Scented Candles: Fire for your Inner Being
         <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Candles,scented ">
-            <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/81FLPQ4ZI7L._AC_UL320_.jpg" alt="link"/></a>
+            <img className="thumb1" id="pic" src="https://m.media-amazon.com/images/I/81FLPQ4ZI7L._AC_UL320_.jpg" alt="link"/></a>
         </div>
-        <div class="card">Astology Candles:Elightenment and empowering
+        <div className="card">Astology Candles:Elightenment and empowering
         <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Candles,astrology ">
-            <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/8140CbFubTL._AC_UL320_.jpg"alt="link" /></a>
+            <img className="thumb1" id="pic" src="https://m.media-amazon.com/images/I/8140CbFubTL._AC_UL320_.jpg"alt="link" /></a>
       </div>
-        <div class="card">Colored Candles:  For your magical needs
+        <div className="card">Colored Candles:  For your magical needs
           <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Colored candles ">
-            <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/71x3xV5X-pL._AC_UL320_.jpg" alt="link"/></a>
+            <img className="thumb1" id="pic" src="https://m.media-amazon.com/images/I/71x3xV5X-pL._AC_UL320_.jpg" alt="link"/></a>
       </div>
-        <div class="card">Unscented Candles: When you just need the fire light
+        <div className="card">Unscented Candles: When you just need the fire light
           <a target="_blank" rel="noopener noreferrer" href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Unscented candles ">
-            <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/51gEH9LPr1L._AC_UL320_.jpg" alt="link"/></a>
+            <img className="thumb1" id="pic" src="https://m.media-amazon.com/images/I/51gEH9LPr1L._AC_UL320_.jpg" alt="link"/></a>
       </div>
-        <div class="card">Bees Wax Candles:An alternative fire source
+        <div className="card">Bees Wax Candles:An alternative fire source
           <a target="_blank"rel="noopener noreferrer"  href="https://www.amazon.com/gp/search?ie=UTF8&tag=brandanew-20&linkCode=ur2&linkId=761157fc8a49cfac385f65d490fca9de&camp=1789&creative=9325&index=hpc&keywords=Bees wax candles ">
-            <img class="thumb1" id="pic" src="https://m.media-amazon.com/images/I/91eFH5ASYZL._AC_UL320_.jpg" alt="link"/></a>
+            <img className="thumb1" id="pic" src="https://m.media-amazon.com/images/I/91eFH5ASYZL._AC_UL320_.jpg" alt="link"/></a>
       </div>
       
     </div>
@@ -38,4 +35,4 @@ class Socials extends React.Component {
     )
   }
 }
-  export default Socials;
+  export default Candles;
